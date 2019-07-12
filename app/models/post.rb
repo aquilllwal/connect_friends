572357cb7b7724d0437c25d99ab5f1886999b873
Aquilll <aquilhussain15@gmail.com>
2019-07-12 12:11:14 +0530

    add picture col to post and install gem carrierwave and mini_magic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :user
+	default_scope -> { order(created_at: :desc) }
+	mount_uploader :picture, PictureUploader
 	validates :user_id, presence: true
 	validates :body, length: {minimum: 1, maximum: 250}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190712051504) do
+ActiveRecord::Schema.define(version: 20190712060328) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.string "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "picture"
   end
 
   create_table "users", force: :cascade do |t|
